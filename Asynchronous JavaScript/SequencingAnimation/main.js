71e32b35de0cf9c92d950fbdaf7e59471ccf7de1
VelazquezClaudia <102626512+VelazquezClaudia@users.noreply.github.com>
2023-05-31 19:45:22 -0700

    Add files

---
+++ Asynchronous JavaScript/SequencingAnimation/main.js
@@ -0,0 +1,51 @@
+const aliceTumbling = [
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' }
+];
+
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: 'forwards'
+}
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+/*alice1.animate(aliceTumbling, aliceTiming);
+alice2.animate(aliceTumbling, aliceTiming);
+alice3.animate(aliceTumbling, aliceTiming);/*
+
+/*function animateAlice() {
+  alice1.animate(aliceTumbling, aliceTiming).finished.then(() => {
+    alice2.animate(aliceTumbling, aliceTiming).finished.then(() => {
+      alice3.animate (aliceTumbling, aliceTiming)
+    });
+  });
+};
+
+animateAlice();*/
+
+/*const alice = alice1.animate(aliceTumbling, aliceTiming);
+
+  alice.finished
+  .then(() => {
+  alice2.animate(aliceTumbling, aliceTiming).finished
+  .then(() => {
+  alice3.animate(aliceTumbling, aliceTiming)
+  .catch((error) => {
+    console.error('Error! Animation not functioning: ${error}');
+    });
+  });
+ });*/
+
+ alice1.animate(aliceTumbling, aliceTiming);
+
+  async function animateAllAlices() {
+  await alice1.animate(aliceTumbling, aliceTiming).finished
+    await alice2.animate(aliceTumbling, aliceTiming).finished
+     alice3.animate(aliceTumbling, aliceTiming)
+    };
+
+animateAllAlices();
